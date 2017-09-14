8da203cd63287c2aa5d8600376da6489bc3703a2
yalotfi <yalotfi@outlook.com.com>
2017-09-13 23:48:14 -0400

    naming convention - cross_entropy

--- assort/utils/cost_functions.py
+++ assort/utils/cost_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def log_loss(y_hat, y):
-    """Negative log-liklihood or categorical cross entropy"""
+def cross_entropy(y_hat, y):
+    """Categorical cross entropy"""
     assert(y_hat.shape == y.shape)
     assert(y_hat.shape[0] == y.shape[0])
     m = y.shape[0]  # Number of training examples
