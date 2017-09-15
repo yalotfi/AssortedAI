1a23a785c72df01d0a02b3e2fab407dbd17e4e63
yalotfi <yalotfi@outlook.com>
2017-09-15 17:54:48 -0400

    add mean squared error cost function

--- assort/utils/cost_functions.py
+++ assort/utils/cost_functions.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+
+def mse(y_hat, y):
+    """Mean Squared Error"""
+    assert(y_hat.shape == y.shape)
+    assert(y_hat.shape[0] == y.shape[0])
+    m = y.shape[0]  # Number of training examples
+    loss = (1 / (2 * m)) * np.sum(np.square(y_hat - y))
+    return loss
+
+
 def cross_entropy(y_hat, y):
     """Categorical cross entropy"""
     assert(y_hat.shape == y.shape)
