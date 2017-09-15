31eb243616c1bf286a2f95a77691a138adcdb9b1
yalotfi <yalotfi@outlook.com>
2017-09-15 17:56:28 -0400

    start linear regression model

--- assort/regression/linear.py
+++ assort/regression/linear.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 # Implement Linear Regression trained with SGD
-# h(w, b) = np.dot(w.T, X) + b
+# h(x) = np.dot(w.T, X) + b
 # J(w, b) = (1 / 2) * np.sum(np.square(h(w, b) - y))
+
+def hypothesis(w, b, X):
+    return np.dot(w, X) + b
+
+def compute_grads(y_hat, y, X):
+    pass
