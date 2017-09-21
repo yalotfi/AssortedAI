8a6b94e63507f47e7961c15f3efc628744f85156
yalotfi <yalotfi@outlook.com>
2017-09-21 17:27:04 -0400

    work on examples

--- examples/restaurant_profit.py
+++ examples/restaurant_profit.py
@@ -9,14 +9,14 @@ from assort.regression.linear import compute_cost
 
 def main():
     # Load data
-    fname = 'house_prices.txt'
+    fname = 'chain_profits.txt'
     fpath = os.path.join('examples', 'datasets', fname)
     data = np.loadtxt(fpath, delimiter=',')
     # Prepare data
-    # X = np.c_[np.ones(data.shape[0]), data[:, 0]]
-    X = data[:, 0:2]
-    y = data[:, -1].reshape((data.shape[0], 1))
-    theta = np.array([[0], [0], [0]])
+    m, n = data.shape[0], 1
+    X = data[:, 0].reshape((m, n))
+    y = data[:, 1].reshape((m, 1))
+    theta = np.array([[0], [0]])
     print(X.shape, y.shape, theta.shape)
     cost, grad = compute_cost(X, y, theta)
     print("Cost: {}".format(cost))
