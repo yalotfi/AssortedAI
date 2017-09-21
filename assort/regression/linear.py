1ac4fafa114e462ad1c778b7e0d16be463a7b417
yalotfi <yalotfi@outlook.com>
2017-09-21 17:28:39 -0400

    work out SGD for linreg implementation

--- assort/regression/linear.py
+++ assort/regression/linear.py
@@ -10,21 +10,11 @@ from assort.cost_functions import mse
 
 def compute_cost(X, y, theta):
     # m = y.size
-    y_hat = hypothesis(X, theta)
+    X = np.c_[np.ones((X.shape[0], 1)), X]
+    y_hat = np.dot(X, theta)
     cost = mse(y_hat, y)
-    grad = mse(y_hat, y, derivative=True, X_train=X)
+    grad = mse(y_hat, y, derivative=True, X=X)
     return (cost, grad)
-
-
-def compute_grads(theta, X, y):
-    m = y.size
-    y_hat = hypothesis(theta, X)
-    return (1 / m) * np.sum(np.multiply(X, (y_hat - y)))
-
-
-def sgd(theta, alpha):
-    grad = compute_grads()
-    return theta - alpha * grad
 
 
 class LinearRegression(object):
@@ -38,39 +28,41 @@ class LinearRegression(object):
         cost_cache -- python list of historical training error
 
     Methods:
-        train -- takes training examples and labels
+        train_sgd -- perform stochastic gradient descent
 
     """
 
     def __init__(self, hyperparamters):
         self.alpha = hyperparamters["learning_rate"]
         self.epochs = hyperparamters["training_iters"]
-        self.cost_cache = np.zeros(self.epochs)
+        self.cost_cache = []
         self.trained_params = {}
-        self.trained_grads = {}
 
     def hypothesis(self, X, theta):
         return np.dot(X, theta)
 
-    def train(self, X_train, y_train):
-        # m = y_train.size
-        theta = np.zeros(())
-        for i in range(self.epochs):
-            y_hat = self.hypothesis(X_train, theta)
-            cost = mse(y_hat, y_train)
-            grad = mse(y_hat, y_train, derivative=True, X=X_train)
-            # dw = (1 / m) * np.dot(X_train.T, (y_hat - y_train))
-            # db = (1 / m) * (y_hat - y_train)
-            # assert(dw.shape == self.w.shape)
-            # assert(db.dtype == float)
-            assert(cost.shape == ())
-            theta -= self.alpha * grad
-        # self.trained_grads = {
-        #     "dw": dw,
-        #     "db": db
-        # }
-        # self.trained_params = {
-        #     "w":
+    def train_sgd(self, X_train, y_train):
+        """Fit OLS Regression with Stochastic Gradient Descent
 
-        #     "b"
-        # }
+        Arguments:
+            X_train -- training feature matrix with shape (m, n)
+            y_train -- training label vector with shape (m, 1)
+
+        Return:
+            self
+        """
+        n = X_train.shape[1]
+        theta = np.zeros((n + 1, 1))
+        X = np.c_[np.ones((theta.shape)), X_train]
+        for i in range(self.epochs):
+            # 1) Make prediction
+            y_hat = self.hypothesis(X, theta)
+            # 2) Compute error of prediction and store it in cache
+            cost = mse(y_hat, y_train)
+            self.cost_cache.append(cost)
+            # 3) Update parameters, theta, by learning rate and gradient
+            grads = mse(y_hat, y_train, derivative=True, X=X)
+            theta -= self.alpha * grads
+        # Save optimized parameters
+        self.trained_params = {"thetas": theta}
+        return self
