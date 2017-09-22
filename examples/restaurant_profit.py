698e982d14afe68d095c2250a77bf000f68184a0
Yaseen Lotfi <yalotfi@outlook.com>
2017-09-21 22:31:50 -0400

    show univariate example of linreg

--- examples/restaurant_profit.py
+++ examples/restaurant_profit.py
@@ -1,27 +1,30 @@
 import sys
 import os
-import numpy as np
 
 # Add AssortedAI to system path for development
 sys.path.insert(0, os.path.join(os.getcwd()))
-from assort.regression.linear import compute_cost
+from assort.regression.linear import LinearRegression
+from assort.utils.load_datasets import get_profit
 
 
 def main():
-    # Load data
-    fname = 'chain_profits.txt'
-    fpath = os.path.join('examples', 'datasets', fname)
-    data = np.loadtxt(fpath, delimiter=',')
-    # Prepare data
-    m, n = data.shape[0], 1
-    X = data[:, 0].reshape((m, n))
-    y = data[:, 1].reshape((m, 1))
-    theta = np.array([[0], [0]])
-    print(X.shape, y.shape, theta.shape)
-    cost, grad = compute_cost(X, y, theta)
-    print("Cost: {}".format(cost))
-    print("Grad: \n{}".format(grad))
-    print(grad.shape)
+    # Load Data
+    (X_train, y_train, X_test) = get_profit()
+
+    # Define model hyperparameters
+    hyperparameters = {
+        "learning_rate": 0.01,
+        "epochs": 1500
+    }
+
+    # Fit Linear Regression model
+    model = LinearRegression(hyperparameters)
+    model = model.train_sgd(X_train, y_train)
+    pred = model.predict(X_test)
+    print(pred)
+
+    thetas = model.trained_params["theta"]
+    print("\nParameters for Univariate OLS:\n{}\n".format(thetas))
 
 
 if __name__ == '__main__':
