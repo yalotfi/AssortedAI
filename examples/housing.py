8edbfc507bbb2fdee2c4e4e737acba30050099eb
Yaseen Lotfi <yalotfi@outlook.com>
2017-09-21 20:48:55 -0400

    housing example edit

--- examples/housing.py
+++ examples/housing.py
@@ -8,14 +8,17 @@ from assort.regression.linear import compute_cost
 
 
 def main():
+    # Load data
     fname = 'housing_uni.txt'
     fpath = os.path.join('examples', 'datasets', 'housing', fname)
     data = np.loadtxt(fpath, delimiter=',')
-    X = np.c_[np.ones(data.shape[0]), data[:, 0]]
-    y = data[:, 1].reshape((data.shape[0], 1))
+    # Prepare data
+    X_train = data[:, 0]
+    y_train = data[:, 1].reshape((data.shape[0], 1))
+    X_train = np.c_[np.ones(data.shape[0]), data[:, 0]]
     theta = np.array([[0], [0]])
-    print(X.shape, y.shape, theta.shape)
-    cost1, cost2 = compute_cost(X, y, theta)
+    print(X_train.shape, y_train.shape, theta.shape)
+    cost1, cost2 = compute_cost(X_train, y_train, theta)
     print(cost1, cost2)
 
 
