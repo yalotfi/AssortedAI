55105ea1715f2fe81a44304f7b279efc8082bdc1
Yaseen Lotfi <yalotfi@outlook.com>
2017-09-22 08:56:16 -0400

    start house price prediction

--- assort/utils/load_datasets.py
+++ assort/utils/load_datasets.py
@@ -17,3 +17,19 @@ def get_profit():
     X_test = np.array([3.5, 7])
 
     return (X_train, y_train, X_test)
+
+
+def get_housing():
+    # Load data
+    fname = 'house_prices.txt'
+    fpath = os.path.join('examples', 'datasets', fname)
+    data = np.loadtxt(fpath, delimiter=',')
+
+    # Prepare data
+    m, n = data.shape[0], data.shape[1] - 1
+    X_train = data[:, 0:2].reshape((m, n))
+    y_train = data[:, -1].reshape((m, 1))
+
+    # Test, dummy data
+    X_test = np.array([[2500, 3], [1000, 2], [1400, 3]])
+    return (X_train, y_train, X_test)
--- examples/housing.py
+++ examples/housing.py
@@ -1,27 +1,18 @@
 import sys
 import os
-import numpy as np
 
 # Add AssortedAI to system path for development
 sys.path.insert(0, os.path.join(os.getcwd()))
-from assort.regression.linear import compute_cost
+from assort.utils import preprocessing
+from assort.regression.linear import LinearRegression
+from assort.utils.load_datasets import get_housing
 
 
 def main():
-    # Load data
-    fname = 'house_prices.txt'
-    fpath = os.path.join('examples', 'datasets', fname)
-    data = np.loadtxt(fpath, delimiter=',')
-    # Prepare data
-    # X = np.c_[np.ones(data.shape[0]), data[:, 0]]
-    X = data[:, 0:2]
-    y = data[:, -1].reshape((data.shape[0], 1))
-    theta = np.array([[0], [0], [0]])
-    print(X.shape, y.shape, theta.shape)
-    cost, grad = compute_cost(X, y, theta)
-    print("Cost: {}".format(cost))
-    print("Grad: \n{}".format(grad))
-    print(grad.shape)
+    X_train, y_train, X_test = get_housing()
+    print(X_train.shape)
+    print(y_train.shape)
+    print(X_test.shape)
 
 
 if __name__ == '__main__':
