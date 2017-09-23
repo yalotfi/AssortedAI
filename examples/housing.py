faaac282531951f08f78f47b2a318cf611dda1e9
Yaseen Lotfi <yalotfi@outlook.com>
2017-09-23 13:22:54 -0400

    complete multivariate OLS example

--- examples/housing.py
+++ examples/housing.py
@@ -1,8 +1,6 @@
 import sys
 import os
 
-import numpy as np
-
 # Add AssortedAI to system path for development
 sys.path.insert(0, os.path.join(os.getcwd()))
 from assort.utils import norm
@@ -17,18 +15,23 @@ def main():
     print(X_test.shape)
 
     # Perform feature scaling on X_train and X_test
-    # X_scale = norm.rescale(X_train, X_test)
-    X_norm = norm.standardize(X_scale)
+    X_norm = norm.standardize(X_train)
+    X_test = norm.standardize(X_test)
 
     # Define model hyperparamters for LinReg
     hyperparameters = {
         "learning_rate": 0.03,
-        "epochs": 200
+        "epochs": 100
     }
 
     # Fit Linear Regression model
     model = LinearRegression(hyperparameters)
-    model = model.train_sgd(X_norm, y_train)
+    model = model.train_sgd(X_norm, y_train, print_cost_freq=10)
+
+    # Visualize training error over each iteration
+    model.plot_error()
+
+    # Make predictions with trained model
     pred = model.predict(X_test)
     print(pred)
 
