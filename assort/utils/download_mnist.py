1280ec6ce2c27d58d940c3e0863a3b2446633ffc
Yaseen Lotfi <yalotfi@outlook.com>
2017-09-26 08:58:11 -0400

    loading mnist from scratch is a pain

--- assort/utils/download_mnist.py
+++ assort/utils/download_mnist.py
@@ -1,59 +1,68 @@
-import numpy as np
 import os
 import gzip
 import struct
+import re
+import numpy as np
 
 from urllib import request
 
-def download_mnist(url, force_download=True):
-    """Download the file from the given url and return the file name."""
-    fname = url.split("/")[-1]
-    if force_download or not os.path.exists(fname):
-        request.urlretrieve(url, fname)
-    return fname
+
+def download_mnist(url, fname, directory):
+    """Download the file from the given url, filename, and directory."""
+    if not os.path.exists(directory):
+        print("Creating directory %s" % directory)
+        os.mkdir(directory)
+    filepath = os.path.join(directory, fname)
+    if not os.path.exists(filepath):
+        print("Downloading %s to %s" % (url, filepath))
+        filepath, _ = request.urlretrieve(url, filepath)
+        statinfo = os.stat(filepath)
+        print("Successfully downloaded", fname, statinfo.st_size, "bytes")
+    return filepath
 
 
-def read_mnist_imgs(fname):
-    """Read and unpack zipped mnist images"""
-    with open(fname, 'rb') as f:
-        magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
-        image = np.fromstring(f.read(), dtype=np.int8)
-    return image
-
-
-def read_mnist_labs(fname):
-    """Read and unpack zipped mnist labels"""
-    with open(fname) as f:
+def read_labels(flab):
+    """Read raw MNIST data files and load them into NumPy arrays."""
+    with gzip.open(flab) as f:
         magic, num = struct.unpack(">II", f.read(8))
-        label = np.fromstring(f.read(), dtype=np.int8).reshape(len(label), 1)
+        labels = np.fromstring(f.read(), dtype=np.int8)
+    return labels
 
 
-def read_data(label_url, image_url):
-    '''
-        Download labels (from the label_url) and images
-        (from the image_url). Load them into Numpy arrays (label and images).
-        Example: label[0] corresponds to label of the image at image[0].
-    '''
-    with gzip.open(download_data(label_url)) as flbl:
-        magic, num = struct.unpack(">II", flbl.read(8))
-        label = np.fromstring(flbl.read(), dtype=np.int8).reshape(len(label), 1)
-
-    with gzip.open(download_data(image_url), 'rb') as fimg:
-        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
-        image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
-    return (label, image)
+def read_mnist(fimg, flab):
+    """Read raw MNIST data files and load them into NumPy arrays."""
+    with gzip.open(flab) as f:
+        magic, num = struct.unpack(">II", f.read(8))
+        labels = np.fromstring(f.read(), dtype=np.int8)
+    m = len(labels)
+    with gzip.open(fimg, 'rb') as f:
+        magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
+        images = np.fromstring(f.read(), dtype=np.uint8)
+    return (images.reshape(m, rows, cols), labels.reshape(m, 1))
 
 
 def main():
-    path='http://yann.lecun.com/exdb/mnist/'
-    # (y_train, X_train) = read_data(
-    #     path+'train-labels-idx1-ubyte.gz', path+'train-images-idx3-ubyte.gz')
-    # (y_test, X_test) = read_data(
-    #     path+'t10k-labels-idx1-ubyte.gz', path+'t10k-images-idx3-ubyte.gz')
-    # print(X_train.shape, y_test.shape)
-    fname = 'train-images-idx3-ubyte.gz'
-    X_train = read_mnist_imgs(fname)
+    # directory = os.path.join('assort', 'datasets', 'mnist')
+    url = 'http://yann.lecun.com/exdb/mnist/'
+    fnames = ['train-images-idx3-ubyte.gz',
+              'train-labels-idx1-ubyte.gz',
+              't10k-images-idx3-ubyte.gz',
+              't10k-labels-idx1-ubyte.gz']
+    for fname in fnames:
+        request.urlretrieve(url, fname)
+    # for fname in fnames:
+    #     if not os.path.exists(directory + fname):
+    #         fpath = download_mnist(url, fname, directory)
+    #         print("File in: ", fpath)
+    #     else:
+    #         print("File %s exists in %s" % (fname, directory))
+    # print(os.path.join(directory, fnames[1]))
+    # y_train = read_labels(os.path.join(directory, fnames[1]))
+    # img_dir = os.path.join(directory, fnames[0])
+    # lab_dir = os.path.join(directory, fnames[1])
+    (X_train, y_train) = read_mnist(fnames[0], fnames[1])
     print(X_train.shape)
+    print(y_train.shape)
 
 
 if __name__ == '__main__':
