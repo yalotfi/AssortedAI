bc771312a770b911875de2878663cbdbc67cd640
yalotfi <yalotfi@outlook.com>
2017-09-28 16:15:48 -0400

    MNIST Reader working

--- assort/utils/mnist_util.py
+++ assort/utils/mnist_util.py
@@ -4,7 +4,6 @@ import gzip
 import struct
 import numpy as np
 
-sys.path.insert(0, os.path.join(os.getcwd()))
 from assort.utils.download_util import download
 
 
@@ -13,25 +12,26 @@ class MNISTReader(object):
     def __init__(self, directory, download_flag=False):
         super(MNISTReader, self).__init__()
         self.directory = directory
-        self.download_flag = download_flag
-        self.fnames = {"X_train": 'train-images-idx3-ubyte.gz',
-                       "y_train": 'train-labels-idx1-ubyte.gz',
-                       "X_test": 't10k-images-idx3-ubyte.gz',
-                       "y_test": 't10k-labels-idx1-ubyte.gz'}
+        self.fnames = {
+            "train_set": {
+                "features": 'train-images-idx3-ubyte.gz',
+                "labels": 'train-labels-idx1-ubyte.gz'
+                },
+            "test_set": {
+                "features": 't10k-images-idx3-ubyte.gz',
+                "labels": 't10k-labels-idx1-ubyte.gz'
+                }
+            }
         self.train_set = self._load_train_set
         self.test_set = self._load_test_set
 
-    def save_mnist(self):
-        """Persist MNIST NumPy Arrays to disk"""
-        pass
-
-    def _download_mnist(self, directory):
-        """Download raw MNIST files from Yann LeCunn's public repo and dumps
-           the gzip files in the given directory.
+    def _download_mnist(self, which_set):
+        """Download train or test MNIST sets from Yann LeCunn's public repo,
+           dumping the gzip files in the given directory.
         """
         url = 'http://yann.lecun.com/exdb/mnist/'
-        for fname in self.fnames:
-            filepath = download(url, fname, directory)
+        for key, fname in which_set.items():
+            filepath = download(url, fname, self.directory)
 
     def _read_labs(self, flab):
         """Unpack label file bytes into NumPy Array"""
@@ -46,40 +46,22 @@ class MNISTReader(object):
         with gzip.open(fimg, 'rb') as f:
             magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
             images = np.fromstring(f.read(), dtype=np.uint8)
-        assert(rows == cols)
-        img_dims = {"row": rows, "col": cols}
-        return images, img_dims
+        assert(rows == cols)  # 28 x 28
+        return images, rows, cols
 
-    def _read_mnist(self, fimg, flab):
-        """Choose to download raw MNIST and return feature/label sets."""
-        img_path = os.path.join(self.directory, fimg)
-        lab_path = os.path.join(self.directory, flab)
-        if self.download_flag:
-            self._download_mnist(self.directory)
+    def _read_mnist(self, which_set):
+        """Try to download raw MNIST and return (feature, label) set."""
+        img_path = os.path.join(self.directory, which_set["features"])
+        lab_path = os.path.join(self.directory, which_set["labels"])
+        self._download_mnist(which_set)
         labels, m = self._read_labs(lab_path)
-        images, dim = self._read_imgs(img_path)
-        return (images.reshape(m, dim["row"], dim["col"]), labels.reshape(m, 1))
+        images, r, c = self._read_imgs(img_path)
+        return (images.reshape(m, r, c), labels.reshape(m, 1))
 
     @property
     def _load_train_set(self):
-        return self._read_mnist(self.fnames["X_train"], self.fnames["y_train"])
+        return self._read_mnist(which_set=self.fnames["train_set"])
 
     @property
     def _load_test_set(self):
-        return self._read_mnist(self.fnames["X_test"], self.fnames["y_test"])
-
-
-def main():
-    directory = os.path.join('assort', 'datasets', 'mnist')
-    reader = MNISTReader(directory)
-    (X_train, y_train) = reader.train_set
-    (X_test, y_test) = reader.test_set
-
-    print(X_train.shape)
-    print(y_train.shape)
-    print(X_test.shape)
-    print(y_test.shape)
-
-
-if __name__ == '__main__':
-    main()
+        return self._read_mnist(which_set=self.fnames["test_set"])
