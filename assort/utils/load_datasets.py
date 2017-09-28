f9655825320236d06980bfc01a17f9a79f7efa4c
yalotfi <yalotfi@outlook.com>
2017-09-28 17:00:03 -0400

    working on mnist loader - binary output

--- assort/utils/load_datasets.py
+++ assort/utils/load_datasets.py
@@ -1,11 +1,9 @@
 import os
-import gzip
-import struct
+import sys
 import time as t
 import numpy as np
 
-from urllib import request
-
+sys.path.insert(0, os.path.join(os.getcwd()))
 from assort.utils.mnist_util import MNISTReader
 
 
@@ -42,24 +40,34 @@ def get_housing():
     return (X_train, y_train, X_test)
 
 
-def get_mnist(directory, binary=False, flatten=True):
+def get_mnist(download=True, serialize=False, binary=False, flatten=False):
+    """Retrieve MNIST datasets and return tuples of train/test sets"""
+
+    # Load MNIST data
     directory = os.path.join('assort', 'datasets', 'mnist')
-    reader = MNISTReader(directory)
+    reader = MNISTReader(directory, download)
     (X_train, y_train) = reader.train_set
     (X_test, y_test) = reader.test_set
 
-    print(X_train.shape)
-    print(y_train.shape)
-    print(X_test.shape)
-    print(y_test.shape)
+    m_train, m_test = X_train.shape[0], X_test[0]
+    px_row, px_col = X_train.shape[1], X_train.shape[2]
+
+    if serialize:
+        fpath = os.path.join(directory, 'mnist.npz')
+        np.savez(fpath,
+                 X_train=X_train, y_train=y_train,
+                 X_test=X_test, y_test=y_test)
+
+    if binary:
+        train_zero_idxs = np.where(y_train == 0)[0]
+        print(train_zero_idxs.shape)
+        print(X_train[train_zero_idxs, :, :].shape)
+        # print(np.take(X_train, train_zero_idxs).shape)
+        # (train_ones_idxs, train_ones_labs) = np.where(y_train == 1)
+        # (test_zero_idxs, test_zero_labs) = np.where(y_test == 0)
+        # (test_ones_idxs, test_ones_labs) = np.where(y_test == 1)
+        # X_train_bin = np.take(X_train, )
 
 
 if __name__ == '__main__':
-    url = 'http://yann.lecun.com/exdb/mnist/'
-    train_img =  'train-images-idx3-ubyte.gz'
-    print(url + train_img)
-
-    # tic = t.time()
-    # X_train = get_mnist()
-    # print(t.time() - tic)
-    # print(X_train.shape)
+    get_mnist(download=False, serialize=False, binary=True, flatten=False)
