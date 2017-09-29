5bc35c2975de14e704b0ad11836c73731d2545e8
yalotfi <yalotfi@outlook.com>
2017-09-29 16:08:47 -0400

    add tanh, relu, and leaky relu

--- assort/activations.py
+++ assort/activations.py
@@ -3,3 +3,18 @@ import numpy as np
 def sigmoid(z):
     """Sigmoid or logistic activation function"""
     return 1 / (1 + np.exp(-z))
+
+
+def tanh(z):
+    """Hyperbolic tangent activation function"""
+    sinh = np.exp(z) - np.exp(-z)
+    cosh = np.exp(z) + np.exp(-z)
+    return sinh / cosh
+
+def relu(z):
+    """Rectified Linear Unit activation function"""
+    return np.maximum(z, 0, z)
+
+def leaky_relu(z, a=0.01):
+    """Leaky Rectified Linear Unit activation function"""
+    return np.maximum(z, a * z, z)
