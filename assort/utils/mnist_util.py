0d8ae62f51b74409c9412a720bb90c0aa7eb2fc9
yalotfi <yalotfi@outlook.com>
2017-09-29 10:40:55 -0400

    add image vectorization functionality to mnist reader

--- assort/utils/mnist_util.py
+++ assort/utils/mnist_util.py
@@ -9,10 +9,11 @@ from assort.utils.download_util import download
 
 class MNISTReader(object):
     """docstring for MNISTReader."""
-    def __init__(self, directory, download_flag=False):
+    def __init__(self, directory, download_flag, flatten_flag):
         super(MNISTReader, self).__init__()
         self.directory = directory
         self.download_flag = download_flag
+        self.flatten_flag = flatten_flag
         self.fnames = {
             "train_set": {
                 "features": 'train-images-idx3-ubyte.gz',
@@ -45,25 +46,35 @@ class MNISTReader(object):
     def _read_imgs(self, fimg):
         """Unpack image file bytes into NumPy Array"""
         with gzip.open(fimg, 'rb') as f:
-            magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
+            magic, m, rows, cols = struct.unpack(">IIII", f.read(16))
             images = np.fromstring(f.read(), dtype=np.uint8)
         assert(rows == cols)  # 28 x 28
         return images, rows, cols
 
-    def _read_mnist(self, which_set):
+    def _pull_set(self, which_set):
         """Try to download raw MNIST and return (feature, label) set."""
+        # Construct path to correct files
         img_path = os.path.join(self.directory, which_set["features"])
         lab_path = os.path.join(self.directory, which_set["labels"])
+
+        # Decide to download files from public repo or not
         if self.download_flag:
             self._download_mnist(which_set)
+
+        # Read in raw MNIST files
         labels, m = self._read_labs(lab_path)
-        images, r, c = self._read_imgs(img_path)
-        return (images.reshape(m, r, c), labels.reshape(m, 1))
+        images, rows, cols = self._read_imgs(img_path)
+
+        # Decide to return vectorized images or not
+        if self.flatten_flag:
+            return (images.reshape(m, rows * cols), labels.reshape(m, 1))
+        else:
+            return (images.reshape(m, rows, cols), labels.reshape(m, 1))
 
     @property
     def _load_train_set(self):
-        return self._read_mnist(which_set=self.fnames["train_set"])
+        return self._pull_set(which_set=self.fnames["train_set"])
 
     @property
     def _load_test_set(self):
-        return self._read_mnist(which_set=self.fnames["test_set"])
+        return self._pull_set(which_set=self.fnames["test_set"])
