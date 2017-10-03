679923293e68dd558d3d6a134e993d28b1c1b159
yalotfi <yalotfi@outlook.com>
2017-10-03 16:49:59 -0400

    commit basic paramater initializer module

--- assort/initializers.py
+++ assort/initializers.py
@@ -2,19 +2,31 @@ import numpy as np
 
 
 class Initializer(object):
-    """docstring for Initializer."""
-    def __call__(self, arg):
-        super(Initializer, self).__init__()
-        self.arg = arg
-
-
-class RandomNorm(Initializer):
-    """docstring for RandomNorm."""
-    def __call__(self, shape):
-        return np.random.rand(shape)
+    """Base initializer class is parent of all initializers."""
+    def __call__(self):
+        raise NotImplementedError
 
 
 class Zeros(Initializer):
-    """docstring for Zeros."""
+    """Initialize parameters as an array of zeros."""
     def __call__(self, shape):
-        self.shape = shape
+        return np.zeros(shape)
+
+
+class Ones(Initializer):
+    """Initialize parameters as an array of ones."""
+    def __call__(self, shape):
+        return np.ones(shape)
+
+
+class RandomNormal(Initializer):
+    """Initialize parameters from a normal random distribution."""
+    def __init__(self, mean=0., stdv=1., seed=None):
+        self.mean = mean
+        self.stdv = stdv
+        self.seed = seed
+
+    def __call__(self, shape, scaling_factor=0.1):
+        if self.seed is not None:
+            np.random.seed(self.seed)
+        return np.random.normal(self.mean, self.stdv, shape) * scaling_factor
