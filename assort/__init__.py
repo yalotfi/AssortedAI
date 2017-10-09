8fc12ced6bb82ce30153773c8e647675414f2865
Yaseen Lotfi <yalotfi@outlook.com>
2017-10-08 22:21:52 -0400

    create config file for cost class

--- assort/__init__.py
+++ assort/__init__.py
@@ -1,7 +1,12 @@
 from assort.initializers import *
+from assort.cost_functions import MeanSquaredError
 
 _INITIALIZER_CONFIG = {
     'zeros': Zeros,
     'ones': Ones,
     'random_normal': RandomNormal
+}
+
+_COST_FUNC_CONFIG = {
+    'mse': MeanSquaredError
 }
