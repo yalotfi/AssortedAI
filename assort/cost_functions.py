58c692f763886050949487c85023907883851412
Yaseen Lotfi <yalotfi@outlook.com>
2017-10-09 22:04:24 -0400

    rewrite cost functions as objects

--- assort/cost_functions.py
+++ assort/cost_functions.py
@@ -1,20 +1,50 @@
 import numpy as np
 
 
-class MeanSquaredError(object):
-    """docstring for MeanSquaredError"""
+class CostFunction(object):
+    """CostFunction: Base class for all cost functions
 
-    def __init__(self, labels, preds, examples=None):
+    Performs basic error handling and inits common attributes
+    """
+
+    def __init__(self, y_train, y_hat, X_train):
+        super(CostFunction, self).__init__()
+        self._check_array_dims(y_train, y_hat, X_train)
+        self.y_train = y_train
+        self.y_hat = y_hat
+        self.loss = self.y_hat - self.y_train
+        self.X_train = X_train
+        self.m = X_train.shape[0]
+
+    def _check_array_dims(self, y_train, y_hat, X_train):
         try:
-            assert(labels.shape == preds.shape)
+            assert(y_train.shape == y_hat.shape)
+            assert(y_train.shape[0] == X_train.shape[0])
         except ValueError:
-            print("Actual and predicted label dimensions must match!")
-        self.m = labels.shape[0]
-        self.y_true = labels
-        self.y_hat = preds
-        self.loss = self.y_hat - self.y_true
-        self.m = self.y_hat.shape[0]
-        self.X = examples
+            print("Array dimensions must match!\n\
+                   y_train: (m, 1),\n\
+                   y_hat: (m, 1),\n\
+                   X_train: (m, n)\n")
+
+
+class MeanSquaredError(CostFunction):
+    """MeanSquaredError
+
+    Arguments:
+        y_train -- actual labels -  vector with shape (m, 1)
+        y_hat -- predicted labels - vector with shape (m, 1)
+        X -- feature matrix, X, with shape (m, n)
+
+    Properties:
+        get_cost: Returns error between predicted and actual labels
+        get_grad: Returns gradient of cost with respect to parameters
+
+    Raises:
+        ValueError: Check dimensions of input arrays
+    """
+
+    def __init__(self, y_train, y_hat, X_train):
+        super().__init__(y_train, y_hat, X_train)
 
     @property
     def get_cost(self):
@@ -22,106 +52,62 @@ class MeanSquaredError(object):
 
     @property
     def get_grad(self):
-        try:
-            assert(self.X is not None)
-        except ValueError:
-            print("Must provide feature matrix, X, to compute derivative!")
-        return (1 / self.m) * np.dot(self.X.T, self.loss)
+        return (1 / self.m) * np.dot(self.X_train.T, self.loss)
 
 
-def mse(y_hat, y_train, derivative=False, X=None):
-    """Mean Squared Error
+class BinaryCrossEntropy(CostFunction):
+    """BinaryCrossEntropy
 
     Arguments:
-        y_hat -- predicted labels - vector with shape (m, 1)
         y_train -- actual labels -  vector with shape (m, 1)
-        derivative -- if true, returns the gradient of the cost function given
-                      the feature matrix
+        y_hat -- predicted labels - vector with shape (m, 1)
         X -- feature matrix, X, with shape (m, n)
 
-    Returns:
-        If derivative -- gradient of MSE cost function
-        Otherwise -- MSE cost between prediction labels and actual labels
+    Properties:
+        get_cost: Returns error between predicted and actual labels
+        get_grad: Returns gradient of cost with respect to parameters
 
     Raises:
-        ValueError: If derivative and X is none b/c X needed to compute
-                    the gradient
+        ValueError: Check dimensions of input arrays
     """
-    try:
-        assert(y_hat.shape[0] == y_train.shape[0])
-        assert(y_hat.shape == y_train.shape)
-    except ValueError:
-        print("predicted and actual label vectors need same dims")
 
-    m = y_train.shape[0]  # Number of training examples
-    loss = y_hat - y_train
-    if derivative:
-        try:
-            assert(X is not None)
-        except ValueError:
-            print("Need to provide feature matrix, X")
-        # return (1 / m) * np.sum(np.dot(X.T, loss))
-        return (1 / m) * np.dot(X.T, loss)
-    else:
-        return (1 / (2 * m)) * np.sum(np.square(loss))
+    def __init__(self, y_train, y_hat, X_train):
+        super().__init__(y_train, y_hat, X_train)
 
+    @property
+    def get_cost(self):
+        case_true = (1 - self.y_train) * np.log(1 - self.y_hat)
+        case_false = self.y_train * np.log(self.y_hat)
+        return -(1 / self.m) * np.sum(case_true + case_false)
 
-def binary_cross_entropy(y_hat, y_train, derivative=False, X=None):
-    """Categorical cross entropy
-
-    Arguments:
-        y_hat -- predicted labels - vector with shape (m, 1)
-        y_train -- actual labels -  vector with shape (m, 1)
-        derivative -- if true, returns the gradient of the cost function given
-                      the feature matrix
-        X -- feature matrix, X, with shape (m, n)
-
-    Returns:
-        If derivative -- Python dictionary of gradients w respect to weights
-                         and biases as follows:
-                            grads = {
-                                "dw": dw,
-                                "db": db
-                            }
-        Otherwise -- Log loss given prediction labels and actual labels
-
-    Raises:
-        ValueError: Need feature matrix, X, to compute the gradient
-    """
-    try:
-        assert(y_hat.shape[0] == y_train.shape[0])
-        assert(y_hat.shape == y_train.shape)
-    except ValueError:
-        print("predicted and actual label vectors need same dims")
-
-    m = y_train.shape[0]  # Number of training examples
-    if derivative:
-        try:
-            assert(X is not None)
-        except ValueError:
-            print("Need to provide feature matrix, X")
-        dZ = y_hat - y_train
-        dw = (1 / m) * np.dot(X.T, dZ)
-        db = (1 / m) * np.sum(dZ)
+    @property
+    def get_grads(self):
+        dw = (1 / self.m) * np.dot(self.X_train.T, self.loss)
+        db = (1 / self.m) * np.sum(self.loss)
         grads = {
             "dw": dw,
             "db": db
         }
         return grads
-    else:
-        return -(1 / m) * np.sum(y_train * np.log(y_hat) + (1 - y_train) * np.log(1 - y_hat))
 
 
-def categorical_cross_entropy(y_hat, y_label, derivative=False, X=None):
-    m = y_label.shape[1]
-    if derivative:
-        dZ = y_label - y_hat
-        dw = -(1 / m) * np.dot(X, dZ.T)
-        db = -(1 / m) * np.sum(dZ)
+class CategoricalCrossEntropy(CostFunction):
+    """CategoricalCrossEntropy"""
+
+    def __init__(self, y_train, y_hat, X_train):
+        super().__init__(y_train, y_hat, X_train)
+
+    @property
+    def get_cost(self):
+        return -(1 / self.m) * np.sum(self.y_hat * np.log(self.y_label))
+
+    @property
+    def get_grads(self):
+        dZ = self.y_label - self.y_hat
+        dw = -(1 / self.m) * np.dot(self.X_train.T, dZ)
+        db = -(1 / self.m) * np.sum(dZ)
         grads = {
             "dw": dw,
             "db": db
         }
         return grads
-    else:
-        return -(1 / m) * np.sum(y_hat * np.log(y_label))
