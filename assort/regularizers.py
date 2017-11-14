83250ec40646ce6fabdddee96bb4497e02ea0730
yalotfi <yalotfi@outlook.com>
2017-11-14 18:00:56 -0500

    add L1 and L2 Regularization

---
+++ assort/regularizers.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+def l1_reg(w, lmda):
+    """
+    L1 Regularization Term
+
+    Arguments
+    ---------
+    w : ndarray
+        Model parameters to decay by regularization term
+    lmda : float
+        Regularization scale, lambda
+
+    Returns
+    -------
+    float
+        Regularization term to add to the cost
+    """
+    return lmda / 2 * np.sum(w)
+
+
+def l2_reg(w, lmda, derivative=False):
+    """
+    L2 Regularization Term
+
+    Arguments
+    ---------
+    w : ndarray
+        Model parameters to decay by regularization term
+    lmda : float
+        Regularization scale, lambda
+    derivative : bool
+        Choose to return derivative of L2 term for backprop. Default is False
+
+    Returns
+    -------
+    float
+        Regularization term to add to the cost
+    """
+    if derivative:
+        return lmda * w
+    else:
+        return lmda / 2 * np.sum(np.dot(w, w.T))
