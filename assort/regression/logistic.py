2c7f3955fabe86aa9107b19304bde4eba669a240
yalotfi <yalotfi@outlook.com>
2017-11-14 18:01:39 -0500

    cleaned up logistic class - need testing and softmax class

--- assort/regression/logistic.py
+++ assort/regression/logistic.py
@@ -2,8 +2,167 @@ import numpy as np
 
 from assort import _INITIALIZER_CONFIG
 from assort.activations import sigmoid
+from assort.activations import softmax
+from assort.regularizers import l2_reg
 from assort.cost_functions import BinaryCrossEntropy
 
+
+class LinearClassifier(object):
+    """Base class for Linear Classifiers"""
+
+    def __init__(self, epochs=1, lr=0.05, lmda=0.01):
+        super(LinearClassifier, self).__init__()
+        # Take in model hyperparameters at object instantiation
+        self._epochs = epochs
+        self._alpha = lr
+        self._lmda = lmda
+
+        # Define hyperparameter dictionary as attribute
+        self.hyperparameters = {
+            "epochs": self._epochs,
+            "learning_rate": self._alpha,
+            "regularization_term": self._lmda
+        }
+
+        # Training attributes
+        self.cost_cache = []
+        self.trained_params = {}
+        self.trained_grads = {}
+
+    def _init_zeros(self, n, k):
+        """
+        Initialize model parameters with zero
+
+        Arguments
+        ---------
+        n : int
+            Define number of features, n
+        k : int
+            Define number of classes, k
+
+        Returns
+        -------
+        ndarray
+            Initialized weights, w, with shapes (n, k)
+        float
+            Initialized bias, b
+        """
+        w = np.zeros((n, k))
+        b = 0.
+        return w, b
+
+    def _binary_xent(self, Y_hat, Y):
+        """
+        Binary Cross Entropy loss function
+
+        Arguments
+        ---------
+        Y_hat : ndarray
+            Probability of y given x
+        Y : ndarray
+            Actual value of y given x
+
+        Returns
+        -------
+        float
+            Cost (or loss) of predicted values
+        """
+        case_0 = Y * np.log(Y_hat)
+        case_1 = (1 - Y) * np.log(1 - Y_hat)
+        return -np.sum(case_true + case_false)
+
+    def _categorical_xent(self, Y_hat, Y):
+        """
+        Categorical Cross Entropy loss function
+
+        Arguments
+        ---------
+        Y_hat : ndarray
+            Probability of y given x
+        Y : ndarray
+            Actual value of y given x
+
+        Returns
+        -------
+        float
+            Cost (or loss) of predicted values
+        """
+        logprobs = np.log(Y_hat)
+        return -np.sum(Y * logprobs)
+
+
+class LogisticRegression(LinearClassifier):
+    """
+    Logistic regression trained on Stochastic Gradient Descent
+
+    Arguments
+    ---------
+    epochs : int
+        Number of full passes to make over dataset when training
+    lr : float
+        Learning rate which determines size of parameter update when training
+    lmda : float
+        Degree to which training cost should be regularized
+
+    Attributes
+    ----------
+    hyperparameters : dictionary
+        Stored hyperparameters for logging
+    trained_params : dictionary
+        Trained parameters
+    trained_grads : dictionary
+        Trained parameter gradients
+    """
+
+    def __init__(self, epochs=1, lr=0.05, lmda=0.01):
+        super().__init__(epochs, lr, lmda)
+
+    def _propagate(self, X, Y, w, b):
+            # Forward Pass
+            Z = np.dot(X, w) + b
+            A = sigmoid(Z)
+            cost = self._binary_xent(A, Y)
+
+            # Backward Pass
+            dZ = A - Y
+            dw = (1 / m) * np.dot(X, dZ.T)
+            db = (1 / m) * np.sum(dZ)
+
+            # Regularize the cost and gradient
+            cost += l2_reg(w, self._lmda)
+            dw += l2_reg(w, self._lmda, derivative=True)
+
+            # Return the cost and gradients
+            grads = {"dw": dw, "db": db}
+            return grads, cost
+
+    def batch_gradient_descent(X, y):
+        # Helpers: dimensionality and classes
+        m, n = X.shape[0], X.shape[1]
+        k = 1
+
+        # Initialize model parameters (weights and bias)
+        w, b = self._init_zeros(n, k)
+
+        for i in range(self._epochs):
+            # Perform single pass of forward and backward propagation
+            cost, grads = self._propagate(X, y, w, b)
+            dw = grads["dw"]
+            db = grads["db"]
+
+            # Update model parameters
+            w = w - self._alpha * dw
+            b = b - self._alpha * db
+
+            # Cache and print model training error
+            cost_cache.append(cost)
+            if i % 100 == 0:
+                print("Cost after iteration {}: {}".format(i, cost))
+
+        # Return trained parameters and their gradients
+        parameters = {"w": w, "b": b}
+        gradients = {"dw": dw, "db": db}
+        return parameters, gradients, cost_cache
 
 class LogisticRegression(object):
     """Logistic regression trained on Stochastic Gradient Descent
