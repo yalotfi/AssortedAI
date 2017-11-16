29f30f44efbc8ad860db300dd2a590cfb52a8be2
yalotfi <yalotfi@outlook.com>
2017-11-16 16:49:25 -0500

    more robust and arbitrary softmax function

--- assort/activations.py
+++ assort/activations.py
@@ -6,10 +6,22 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-def softmax(z):
-    """Softmax activation function (generalized sigmoid)"""
-    z -= np.max(z)
-    return np.exp(z) / np.sum(np.exp(z), axis=0)
+# def softmax(z):
+#     """Softmax activation function (generalized sigmoid)"""
+#     z -= np.max(z)
+#     return np.exp(z) / np.sum(np.exp(z), axis=0)
+
+def softmax(arr, axis=None):
+    X = np.atleast_2d(arr)
+    if axis is None:
+        axis = next(j[0] for j in enumerate(X.shape) if j[1] > 1)
+    X = X - np.expand_dims(np.max(X, axis=axis), axis)
+    X = np.exp(X)
+    probs = X / np.expand_dims(np.sum(X, axis=axis), axis)
+    if len(arr.shape) == 1:
+        return probs.flatten()
+    else:
+        return probs
 
 
 def tanh(z):
