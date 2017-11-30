a036bac773f1458b6600d11426b6db1c64f9091d
YLotfi <yaseen.lotfi@fticonsulting.com>
2017-11-30 17:12:16 -0500

    rename class to DeepNeuralNetwork and add documentation

--- assort/neural/mlp.py
+++ assort/neural/mlp.py
@@ -1,21 +1,73 @@
 import numpy as np
 
+from assort.initializers import RandomNormal
+from assort.initializers import Zeros
 
-class MultiLayerPerceptron(object):
-    """docstring for MultiLayerPerceptron."""
+
+class DeepNeuralNetwork(object):
+    """docstring for DeepNeuralNetwork."""
     def __init__(self, layer_dims):
-        super(MultiLayerPerceptron, self).__init__()
+        super(DeepNeuralNetwork, self).__init__()
         self.layer_dims = layer_dims
         self.L = len(self.layer_dims)
         self.parameters = self._get_params
 
     def _initialize_parameters(self, layer_dims):
+        """
+        Initialize model parameters with random normal distribution
+
+        Arguments
+        ---------
+        layer_dims : list
+            Each element is the input dimension of the corresponding layer
+
+        Returns
+        -------
+        dict
+            Python dictionary containing initialized weights and biases for
+            each layer of the model
+        """
+        rand_init = RandomNormal()
+        zero_init = Zeros()
         parameters = {}
         for l in range(1, self.L):
-            parameters['W' + str(l)] = np.random.randn(
-                layer_dims[l], layer_dims[l - 1]) * 0.01
-            parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+            weight_shape = (layer_dims[l], layer_dims[l - 1])
+            bias_shape = (layer_dims[l], 1)
+            parameters['W' + str(l)] = initializer(weight_shape)
+            parameters['b' + str(l)] = zero_init(bias_shape)
         return parameters
+
+    def _linear_forward(A, W, b):
+        """
+        Implement a single linear transformation during forward propagation
+
+        Arguments
+        ---------
+        A : ndarray
+            The previous layer's activations are inputs to the current layer.
+                - Size: (hh_prev, m_examples)
+        W : ndarray
+            The current layer's weights
+                - Size: (hh_curr, hh_prev)
+        b : ndarray
+            The current layer's bias units
+                - Size: (hh_curr, 1)
+
+        Returns
+        -------
+        ndarray
+            The input to an activation function of the current layer
+        dict
+            Python dictionary caching the previous activations, layer weights
+            and bias which will be used when computing back propagation
+        """
+        Z = np.dot(W, A) + b
+        cache = A, W, b
+        return A, cache
+
+    def _forward_activation(A_prev, W, b, activation_func):
+        Z, linear_cache = self._linear_forward(A_prev, W, b)
+        A = sigmoid(Z)
 
     @property
     def _get_params(self):
